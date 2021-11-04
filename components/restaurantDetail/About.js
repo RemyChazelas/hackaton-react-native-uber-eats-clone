334c123cd18fc7f0641b7bec80205a5d7e59c8ee
RemyChazelas <remychazelas@gmail.com>
2021-11-04 18:06:15 +0100

    create the about file

--- components/restaurantDetail/About.js
+++ components/restaurantDetail/About.js
@@ -1,10 +1,47 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 
 export default function About() {
+
+    const image = "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+    const name = "FarmHouse Kitchen Thai Cuisine"
+    const desc = "thai • Food • $$ • 🎫 • 4 ⭐ (2913+)"
+
     return (
         <View>
-            <Text>about</Text>
+            <RestaurantImage image={image} />
+            <RestaurantName name={name} />
+            <RestaurantDescription desc={desc} />
         </View>
     )
 }
+
+const RestaurantImage = (props) => (
+    <Image source={{ uri: props.image }} style={{ width: "100%", height: 180 }} />
+);
+
+const RestaurantName = (props) => (
+    <Text
+        style={{
+            fontSize: 29,
+            fontWeight: "600",
+            marginTop: 10,
+            marginHorizontal: 15,
+        }}
+    >
+        {props.name}
+    </Text>
+);
+
+const RestaurantDescription = (props) => (
+    <Text
+        style={{
+            marginTop: 10,
+            marginHorizontal: 15,
+            fontWeight: "400",
+            fontSize: 15.5,
+        }}
+    >
+        {props.desc}
+    </Text>
+);
--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { Divider } from 'react-native-elements'
+import About from '../components/restaurantDetail/About'
 
 export default function RestaurantDetail() {
     return (
         <View>
-            <Text></Text>
+            <About />
+            <Divider width={1.8} style={{ marginVertical: 20 }} />
         </View>
     )
 }
