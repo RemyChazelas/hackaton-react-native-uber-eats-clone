d9c0a6892da34aa2b5b77fb21999013b279a296e
RemyChazelas <remychazelas@gmail.com>
2021-11-04 15:04:47 +0100

    add restaurantItem component and his style

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import Categories from '../components/Categories'
 import HeaderTabs from '../components/HeaderTabs'
+import RestaurantItems from '../components/RestaurantItems'
 import SearchBar from '../components/SearchBar'
 
 export default function Home() {
@@ -11,7 +12,11 @@ export default function Home() {
                 <HeaderTabs />
                 <SearchBar />
             </View>
-            <Categories />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Categories />
+                <RestaurantItems />
+
+            </ScrollView>
         </SafeAreaView>
     )
 }
