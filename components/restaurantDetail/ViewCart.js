e1bd324efc9e52390b163dc93eb505f7820ac998
RemyChazelas <remychazelas@gmail.com>
2021-11-05 01:02:11 +0100

    add modal for checkout but it's not display :((((((((((

--- components/restaurantDetail/ViewCart.js
+++ components/restaurantDetail/ViewCart.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity, Modal } from 'react-native'
 import { useSelector } from "react-redux";
 
 export default function ViewCart() {
+
+    const [modalVisible, setModalVisible] = useState(false);
 
 
     const { items } = useSelector(
@@ -18,10 +20,38 @@ export default function ViewCart() {
         currency: "USD",
     });
 
-    console.log(totalUSD);
+
+    const checkoutModalContent = () => {
+        <View style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+        }}>
+            <View style={{
+                backgroundColor: "black",
+                padding: 10,
+                borderRadius: 30,
+                width: 150,
+                alignItems: "center",
+            }}>
+                <TouchableOpacity onPress={() => setModalVisible(false)}>
+                    <Text style={{ color: "white" }}>checkout</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    }
+
 
     return (
         <>
+            <Modal
+                animationType="slide"
+                visible={modalVisible}
+                transparent={true}
+                onRequestClose={() => setModalVisible(false)}
+            >
+                {checkoutModalContent()}
+            </Modal>
             {total ? (
                 <View
                     style={{
@@ -53,7 +83,9 @@ export default function ViewCart() {
                                 width: 300,
                                 position: "relative",
                             }}
-
+                            onPress={() => {
+                                setModalVisible(true);
+                            }}
                         >
                             <Text style={{ color: "white", fontSize: 20, marginRight: 30 }}>view cart</Text>
                             <Text style={{ color: "white", fontSize: 20 }}>${totalUSD}</Text>
